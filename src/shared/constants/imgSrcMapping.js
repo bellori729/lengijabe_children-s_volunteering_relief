a91a05190966418cb603098c0a4775e8a6b4d561
bellori729 <bellori729@gmail.com>
2025-11-09 00:51:41 +0300

    feat : 이미지 소스 맵핑 추가

---
+++ src/shared/constants/imgSrcMapping.js
@@ -0,0 +1,7 @@
+const IMG_SRC_MAPPING = {
+  logo: "/assets/images/logo.png",
+  logo_clear: "/assets/images/logo_clear.png",
+  logo_vector: "/assets/images/logo_vector.svg",
+};
+
+export default IMG_SRC_MAPPING;
