97b06fa637c8428ca6fe9a5759b0272e445ef046
bellori729 <bellori729@gmail.com>
2025-11-09 00:24:21 +0300

    feat : 스크롤 최상단 이동 커스텀 훅 추가 및 App.jsx 랜더링 시 실행

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import Router from "./router/Router";
+import { useScrollToTop } from "./shared/hooks/useScrollToTop";
 
 const App = () => {
+  useScrollToTop();
+
   return <Router />;
 };
 
