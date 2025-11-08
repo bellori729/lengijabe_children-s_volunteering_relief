79a96493534bc5ceb753fbe3ed2646a6bb5be8b6
bellori729 <bellori729@gmail.com>
2025-11-09 00:51:53 +0300

    feat : 폰트 스타일 상수 파일 추가

---
+++ src/shared/constants/fontStyle.js
@@ -0,0 +1,23 @@
+// ========== small ========== //
+export const smallTextRegular =
+  "font-basic text-[12px] leading-[150%] font-[400]";
+
+export const smallTextBold = `${smallTextRegular} font-[700]`;
+
+// ========== normal ========== //
+export const normalTextRegular =
+  "font-basic text-[16px] leading-[150%] font-[400]";
+
+export const normalTextBold = `${normalTextRegular} font-[700]`;
+
+// ========== medium ========== //
+export const mediumTextRegular =
+  "font-basic text-[22px] leading-[150%] font-[400]";
+
+export const mediumTextBold = `${mediumTextRegular} font-[700]`;
+
+// ========== large ========== //
+export const largeTextRegular =
+  "font-basic text-[38px] leading-[150%] font-[400]";
+
+export const largeTextBold = `${largeTextRegular} font-[700]`;
