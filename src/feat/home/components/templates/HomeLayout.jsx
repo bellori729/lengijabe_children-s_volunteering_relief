e6558301feced9eea1a93b30f8e37e856699e50d
bellori729 <bellori729@gmail.com>
2025-11-09 01:50:38 +0300

    feat : 로고 추가

--- src/feat/home/components/templates/HomeLayout.jsx
+++ src/feat/home/components/templates/HomeLayout.jsx
@@ -1,9 +1,15 @@
 import BasicTemplate from "../../../../shared/components/templates/BasicTemplate";
+import IMG_SRC_MAPPING from "../../../../shared/constants/imgSrcMapping";
+import classNameMerge from "../../../../utils/classNameMerge";
 
-const HomeLayout = ({ className, style, children }) => {
+const HomeLayout = ({ className, style }) => {
   return (
     <BasicTemplate className={className} style={style}>
-      {children}
+      <img
+        className={classNameMerge(["w-[35%] h-[35%]"])}
+        src={IMG_SRC_MAPPING.logo_vector}
+        alt="Logo"
+      />
     </BasicTemplate>
   );
 };
