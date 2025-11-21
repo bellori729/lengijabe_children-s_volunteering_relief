61587bf8b7ddea2e3fe428704523d59994c5e9fe
bellori729 <bellori729@gmail.com>
2025-11-21 13:13:28 +0300

    feat : The Organizations Main Goals 영역 마무리

--- src/feat/home/components/molecules/HomeTheOrganizationsMainGoals.jsx
+++ src/feat/home/components/molecules/HomeTheOrganizationsMainGoals.jsx
@@ -1,4 +1,3 @@
-import { mediumTextBold } from "../../../../shared/constants/fontStyle";
 import classNameMerge from "../../../../utils/classNameMerge";
 import HomeContentsTitle from "../atoms/HomeContentsTitle";
 import HomeContentsContainer from "../organisms/HomeContentsContainer";
@@ -16,6 +15,34 @@ const HomeTheOrganizationsMainGoals = () => {
         <li>
           Provision of formal and informal education, as well as vocational
           training to children and youths.
+        </li>
+        <li>
+          Socializing street children and youths to make them fit in society as
+          responsible citizens.
+        </li>
+        <li>
+          Promote community awareness and carrying out advocacy on matters
+          related to street children including children rights
+        </li>
+        <li>
+          Networking with other organizations and institutions for sharing
+          information and experience, as well as collaborating on interventions
+          that are geared to improve performance.
+        </li>
+        <li>
+          Enhancing the institutional capacity of the organization for effective
+          and efficient advancement of its mission.
+        </li>
+        <li>
+          Apart from basic needs, children and youth are given access to
+          opportunities for learning life-skills; engage in sports, gardening,
+          art work, craft and religion- they are provided on individualized
+          holistic basis.
+        </li>
+        <li>
+          The disadvantaged children are sensitized to stay away from HIV/AIDS
+          pandemic, while orphans access accommodation, street children enjoy
+          opportunities to education and training.
         </li>
       </ul>
     </HomeContentsContainer>
